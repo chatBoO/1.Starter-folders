ed90e2bfb87ce78b5381695262fdf8d2c9442f02
Mathieu SEGAUD <segaud.mathieu@gmail.com>
2024-11-14 14:27:15 +0100

    Commencement du projet gradient

--- 6.gradient-maker/src/App.jsx
+++ 6.gradient-maker/src/App.jsx
@@ -1,5 +1,44 @@
+import Gradient from "./components/Gradient";
+import { useSelector } from "react-redux";
+import ColorInput from "./components/inputs/ColorInput";
+
 function App() {
-  return <div></div>
+    const gradientValues = useSelector((state) => state.gradient);
+
+    return (
+        <div className="relative text-slate-100 max-w-xl mx-auto mt-20 p-4 border border-slate-400 flex">
+            <div className="w-1/2 p-4 pr-8">
+                <h1 className="text-center text-xl">Gradient Generator</h1>
+                <p className="text-center mb-6">Bring style to your apps.</p>
+
+                <p className="mb-1">Colors, min 2, max 5.</p>
+                <div className="flex mb-2">
+                    {gradientValues.colors.map((obj) => (
+                        <ColorInput
+                            id={obj.id}
+                            color={obj.value}
+                            key={obj.id}
+                        />
+                    ))}
+                </div>
+                <div className="mb-5 =">
+                    <button>-</button>
+                    <button>+</button>
+                </div>
+                <p>Pick and change a color's position</p>
+                {/* selectColor */}
+
+                <p>Color's position</p>
+                {/* rangeColor */}
+
+                <p>Gradient global angle</p>
+                {/* rangeAngle */}
+
+                {/* openModal8tn */}
+            </div>
+            <Gradient />
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ 6.gradient-maker/src/features/gradient.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    colors: [
+        {
+            id: 1,
+            value: "#00d2ff",
+            position: 20,
+        },
+        {
+            id: 2,
+            value: "#ee9ca7",
+            position: 50,
+        },
+    ],
+    pickedColorId: 1,
+    angle: 60,
+};
+
+export const gradientSlice = createSlice({
+    name: "gradient",
+    initialState,
+    reducers: {
+        updateColorValue: (state, action) => {
+            const currentColor = state.colors.find(
+                (color) => color.id === action.payload.id
+            );
+            currentColor.value = action.payload.value;
+        },
+    },
+});
+
+export const { updateColorValue } = gradientSlice.actions;
+export default gradientSlice.reducer;
